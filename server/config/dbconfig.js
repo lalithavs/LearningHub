b57d5ae48fbff0dd26ecbfd8b8d715044e41a08e
Lalitha V S <lalithavs@MacBook-Pro-16.local>
2020-08-07 21:43:18 +0530

    Changed Db connection string

--- server/config/dbconfig.js
+++ server/config/dbconfig.js
@@ -1,7 +1,8 @@
 "use strict";
 module.exports = class DBConfig {
   constructor() {
-    this.dbUrl =
-      "mongodb+srv://lalitha:lalitha@cluster0-np8bq.mongodb.net/learninghub?retryWrites=true&w=majority";
+    // this.dbUrl =
+    //   "mongodb+srv://lalitha:lalitha@cluster0-np8bq.mongodb.net/learninghub?retryWrites=true&w=majority";
+    this.dbUrl = "mongodb://localhost:27017/learninghub";
   }
 };
